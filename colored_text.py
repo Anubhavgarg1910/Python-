a9288dc755e888c6dc507ab2ef44e3f2c48909da
Anubhav Garg <71034578+Anubhavgarg1910@users.noreply.github.com>
2022-06-10 08:23:12 +0530

    Create colored_text.py

---
+++ colored_text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Anubhav Garg "+ Fore.YELLOW+ Back.BLUE+"I am a Machine Learning Enthusiast")
+print(Back.CYAN+"I am a student.")
+print(Fore.RED + Back.GREEN+ "I like to play badminton.")
